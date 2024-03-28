88d2cfd0d169f43def488e0b9303f4ddea4fa273
Justin Ikeako <justinikeako@gmail.com>
2024-03-28 16:10:25 -0500

    Switch biggest demo to spring animations

--- src/app/biggest/page.tsx
+++ src/app/biggest/page.tsx
@@ -7,11 +7,11 @@ export default function Page() {
         <span className="sr-only">Toggle</span>
       </label>
 
-      <h1 className="relative flex -translate-y-[.25em] translate-x-[.5em] transform items-center justify-start text-6xl transition-transform duration-1000 -rotate-x-30 rotate-y-45 group-has-[:checked]:-translate-x-[.5em] group-has-[:checked]:-translate-y-0 group-has-[:checked]:-rotate-y-30 group-has-[:checked]:rotate-x-30 group-has-[:checked]:rotate-z-12 group-has-[:checked]:translate-z-32 sm:text-7xl sm:group-has-[:checked]:-translate-x-[.75em] md:text-8xl lg:text-9xl lg:group-has-[:checked]:-translate-x-[1em]">
-        <span className="block origin-left text-[3.5em] font-semibold tracking-tighter transition-transform duration-1000 group-has-[:checked]:scale-x-0 group-has-[:checked]:scale-y-[0.3]">
+      <h1 className="relative flex -translate-y-[.25em] translate-x-[.5em] transform items-center justify-start text-6xl transition-transform -rotate-x-30 rotate-y-45 ease-spring spring-[1500/0] group-has-[:checked]:-translate-x-[.5em] group-has-[:checked]:-translate-y-0 group-has-[:checked]:-rotate-y-30 group-has-[:checked]:rotate-x-30 group-has-[:checked]:rotate-z-12 group-has-[:checked]:translate-z-32 sm:text-7xl sm:group-has-[:checked]:-translate-x-[.75em] md:text-8xl lg:text-9xl lg:group-has-[:checked]:-translate-x-[1em]">
+        <span className="block origin-left text-[3.5em] font-semibold tracking-tighter transition-transform ease-spring spring-[1500/0] group-has-[:checked]:scale-x-0 group-has-[:checked]:scale-y-[0.3]">
           Big
         </span>
-        <span className="absolute block origin-right scale-x-0 scale-y-[3.5] text-nowrap text-[1em] font-semibold tracking-tighter transition-transform duration-1000 group-has-[:checked]:scale-x-100 group-has-[:checked]:scale-y-100">
+        <span className="absolute block origin-right scale-x-0 scale-y-[3.5] text-nowrap text-[1em] font-semibold tracking-tighter transition-transform ease-spring spring-[1500/0] group-has-[:checked]:scale-x-100 group-has-[:checked]:scale-y-100">
           and bigger
         </span>
       </h1>
